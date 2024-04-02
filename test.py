cf6c050d32d6807ecc828a93f1a56b97068b0f58
Aidan Phillips <aidanphillips21@outlook.com>
2024-04-02 16:43:48 -0400

    test file

---
+++ test.py
@@ -0,0 +1,26 @@
+from ProtFlash.pretrain import load_prot_flash_small
+from ProtFlash.utils import batchConverter
+import torch
+from ProtFlash.model import FLASHTransformer
+
+
+MODEL_URL_SMALL = "https://zenodo.org/record/7655858/files/flash_protein.pt"
+data = [
+    ("protein1", "MKTVRQERLKSIVRILERSKEPVSGAQLAEELSVSRQVIVQDIAYLRSLGYNIVATPRGYVLAGG"),
+    ("protein2", "KALTARQQEVFDLIRDHISQTGMPPTRAEIAQRLGFRSPNAAEEHLKALARKGVIEIVSGASRGIRLLQEE"),
+]
+ids, batch_token, lengths = batchConverter(data)
+model = load_prot_flash_small()
+with torch.no_grad():
+    token_embedding = model(batch_token, lengths)
+# Generate per-sequence representations via averaging
+sequence_representations = []
+for i, (_, seq) in enumerate(data):
+    sequence_representations.append(token_embedding[i, 0: len(seq) + 1].mean(0))
+
+model_data = torch.load(MODEL_URL_SMALL)
+hyper_parameter = model_data["hyper_parameters"]
+model = FLASHTransformer(hyper_parameter['dim'], hyper_parameter['num_tokens'], hyper_parameter         ['num_layers'], group_size=hyper_parameter['num_tokens'],
+                             query_key_dim=hyper_parameter['qk_dim'], max_rel_dist=hyper_parameter['max_rel_dist'], expansion_factor=hyper_parameter['expansion_factor'])
+
+model.load_state_dict(model_data['state_dict'])
