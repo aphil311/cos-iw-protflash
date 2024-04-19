bdd4500e8b31bbe024e870756605be00283529c2
Aidan Phillips <aidanphillips21@outlook.com>
2024-04-19 18:44:55 -0400

    trains

--- test.py
+++ test.py
@@ -14,18 +14,25 @@ ss_dict = {'H':0, 'B':1, 'E':2, 'G':3, 'I':4, 'T':5, 'S':6, 'C':7}
 
 # MODEL_URL_SMALL = "/scratch/network/ap9884/flash_protein.pt"
 # MODEL_URL_SMALL = "./pretrained-models/flash_protein.pt"
+examples = 128
+training_steps = 256
+
+# set device to be gpu
+device = torch.device("cuda")
 
 # print('building dataframe...')
 # read in proteins
-examples = 8
-training_steps = 10
 # df = pd.read_csv('/Users/aidan/Documents/COS 398/archive/PDB_31-07-2011.csv', nrows=8)
-df = pd.read_csv('/scratch/network/ap9884/PDB_31-07-2011.csv', nrows=8)
+df = pd.read_csv('/scratch/network/ap9884/PDB_31-07-2011.csv', nrows=1024)
 
-plm_model = load_prot_flash_small()
-model = Convolution_Predictor(512)
+plm_model = load_prot_flash_small().to(device)
+model = Convolution_Predictor(512).to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.AdamW(model.parameters())
+
+# checkpoint = torch.load('/scratch/network/ap9884/model.pt')
+# model.load_state_dict(checkpoint['model_state_dict'])
+# optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
 # training loop
 print('starting training loop...')
@@ -47,7 +54,7 @@ for s in range(training_steps):
     max_len = max(lengths)
 
     # populate the data and generate masks
-    masks = torch.full((examples, 256, max_len), True, dtype=torch.bool)
+    masks = torch.full((examples, 256, max_len), True, dtype=torch.bool).to(device)
     i = 0
     for index, row in batch.iterrows():
         pdf_id = row['pdb_id']
@@ -59,14 +66,14 @@ for s in range(training_steps):
     # print('getting embeddings...')
     ids, batch_token, lengths = batchConverter(data)
     with torch.no_grad():
-        token_embedding = plm_model(batch_token, lengths)
+        token_embedding = plm_model(batch_token.to(device), lengths.to(device))
 
     # go from (N, L, d) to (N, d, L)
     token_embedding = torch.transpose(token_embedding, 1, 2)
 
     # Build the mask vector
     # print('getting secondary structures...')
-    output = model(token_embedding, masks)
+    output = model(token_embedding, masks.to(device))
     # trimmed_output = []
     # pretty_output = []
     # i=0
@@ -76,7 +83,7 @@ for s in range(training_steps):
     #     i += 1
 
     # get a matrix (N, L, 8) that is one-hot
-    targets = torch.full((examples, 8, max_len), 0, dtype=float)
+    targets = torch.full((examples, 8, max_len), 0, dtype=float).to(device)
     i=0
     for protein in labels:
         j=0
@@ -109,3 +116,40 @@ for s in range(training_steps):
     #     # print(labels[i][1])
     # if s == 10 or s == 9:
     #     print('epoch: ', s+1,' loss: ', loss.item())
+
+df = pd.read_csv('/scratch/network/ap9884/PDB_31-07-2011.csv', nrows=1)
+
+data = []
+for index, row in df.iterrows():
+    pdf_id = row['pdb_id']
+    seq = row['seq']
+    l = len(seq)
+    data.append((pdf_id, seq))
+
+masks = torch.full((1, 256, l), True, dtype=torch.bool).to(device)
+
+print('getting embeddings...')
+ids, batch_token, lengths = batchConverter(data)
+with torch.no_grad():
+    token_embedding = plm_model(batch_token.to(device), lengths.to(device))
+
+# go from (N, L, d) to (N, d, L)
+token_embedding = torch.transpose(token_embedding, 1, 2)
+
+print('getting secondary structures...')
+output = torch.transpose(model(token_embedding, masks.to(device)), 1, 2)
+output_labels = torch.argmax(output, dim=2)
+idx_to_label = {v:k for k, v in ss_dict.items()}
+# print(output_labels[0])
+# final_label_0 = [idx_to_label[i] for i in output_labels[0].cpu().numpy()]
+# string = ""
+# print(string.join(final_label_0))
+for l in output_labels:
+    final = [idx_to_label[i] for i in l.cpu().numpy()]
+    string = ""
+    print(string.join(final))
+
+torch.save({
+    'model_state_dict': model.state_dict(),
+    'optimizer_state_dict': optimizer.state_dict(),
+    }, '/scratch/network/ap9884/model.pt')
